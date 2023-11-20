94d2a9858323885e66c8d6a6e9111ae8d1195259
osawa-koki <koki@osawa.dev>
2023-11-20 21:24:05 +0900

    `QueryClient`の設定。

--- client/pages/fruits.tsx
+++ client/pages/fruits.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function FruitsPage(): React.JSX.Element {
+export default function FruitsPage (): React.JSX.Element {
   return (
     <div>
       <h1>Fruits</h1>
