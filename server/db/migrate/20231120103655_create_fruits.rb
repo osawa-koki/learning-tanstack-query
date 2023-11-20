0d32a6132b2972ecc83542345e903b90d3b4172f
osawa-koki <koki@osawa.dev>
2023-11-20 19:37:44 +0900

    rails g model Fruit name:string position:integer comment:string

---
+++ server/db/migrate/20231120103655_create_fruits.rb
@@ -0,0 +1,11 @@
+class CreateFruits < ActiveRecord::Migration[7.0]
+  def change
+    create_table :fruits do |t|
+      t.string :name
+      t.integer :position
+      t.string :comment
+
+      t.timestamps
+    end
+  end
+end
