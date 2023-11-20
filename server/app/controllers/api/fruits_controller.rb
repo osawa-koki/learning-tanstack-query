e4161b87612da9a861d53b3cffd0deda06895ea5
osawa-koki <koki@osawa.dev>
2023-11-20 20:58:27 +0900

    CRUDのAPIの作成。

--- server/app/controllers/api/fruits_controller.rb
+++ server/app/controllers/api/fruits_controller.rb
@@ -1,2 +1,40 @@
 class Api::FruitsController < ApplicationController
+  before_action :set_fruit, only: [:show, :update, :destroy]
+
+  def index
+    fruits = Fruit.all
+    render json: fruits
+  end
+
+  def show
+    render json: @fruit
+  end
+
+  def create
+    fruit = Fruit.new(fruit_params)
+    if fruit.save
+      render json: fruit
+    else
+      render json: fruit.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @fruit.update(fruit_params)
+      render json: fruit
+    else
+      render json: fruit.errors, status: unprocessable_entity
+    end
+  end
+
+  def destroy
+    @fruit.destroy
+    render json: fruit
+  end
+
+  private
+
+  def set_fruit
+    @fruit = Fruit.find(params[:id])
+  end
 end
